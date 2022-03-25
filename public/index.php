a3c1691f8d2eead456d53f0344befbb507903eb3
Antoine <louchardantoine@gmail.com>
2022-03-25 14:54:54 +0100

    First commit

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+
+use Framework\App;
+
+require '../vendor/autoload.php';
+
+$app = new App();
+
+$response = $app->run(\GuzzleHttp\Psr7\ServerRequest::fromGlobals());
+\Http\Response\send($response);
